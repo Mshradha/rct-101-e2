1196f4d2afb90a62c2b69767fecd6bb40814642b
Mshradha <99673738+Mshradha@users.noreply.github.com>
2022-05-30 16:59:16 +0530

    sss

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 
-const Pagination = () => {
+const Pagination = ({page,setPage, setLimit}) => {
   // TODO: Remove below const and instead import them from chakra
-  const Button = () => <div />;
-  const ButtonGroup = () => <div />;
-  const Select = () => <div />;
+  // const Button = () => <div />;
+  // const ButtonGroup = () => <div />;
+  // const Select = () => <div />;
 
+  const handleChanges=(e)=>{
+    setLimit(e.target.value);
+    console.log(e.target.value)
+  }
   return (
     <ButtonGroup>
-      <Button data-cy="pagination-first-button"></Button>
-      <Button data-cy="pagination-previous-button"></Button>
-      <Select data-cy="pagination-limit-select">
-        <option data-cy="pagination-limit-3"></option>
-        <option data-cy="pagination-limit-6"></option>
-        <option data-cy="pagination-limit-9"></option>
+      <Button disabled ={page<=1} onClick = {()=> setPage(1)} data-cy="pagination-first-button">First</Button>
+      <Button disabled = {page<=1} onClick={()=> setPage(page-1)} data-cy="pagination-previous-button">Previous</Button>
+      <Select data-cy="pagination-limit-select" onChange={handleChanges}>
+        <option data-cy="pagination-limit-3">3</option>
+        <option data-cy="pagination-limit-6">6</option>
+        <option data-cy="pagination-limit-9">9</option>
       </Select>
-      <Button data-cy="pagination-next-button"></Button>
-      <Button data-cy="pagination-last-button"></Button>
+      <Button onClick={()=> setPage(page+1)} data-cy="pagination-next-button">Next</Button>
+      <Button onClick={()=> setPage(1)} data-cy="pagination-last-button">Last</Button>
     </ButtonGroup>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+
+import React, {useEffect , useState } from "react";
+import Pagination from "./Pagination";
+import {Flex,Grid,Spacer,GridItem,Square} from "@chakra-ui/react";
+import Product from "./Product";
+import AddProduct from "./AddProduct"
+import axios from "axios";
+
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
+  const[page , setPage] = useState(1);
+  const [limit , setLimit] = useState(3);
+  const [data,setData] = useState([]);
+
+  useEffect(()=>{
+    const getData = async()=>{
+      let res = await axios.get(`http://localhost:8080/products?_page=${page}&_limit=${limit}`);
+      console.log(res.data);
+      setData(res.data)
+    }
+    getData()
+  },[page, limit])
 
   return (
     <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
-      {/* Pagination */}
+      <Square><AddProduct/></Square>
+
+      <Spacer>
+     
+      <Grid h='200px' templateRows='repeat(2,1fr)' templateColumns='repeat(5,1fr)' gap={4}>{<GridItem>{data.map((el)=> <Product key={el.id} data={el}/>)}
+        </GridItem>}</Grid>
+        </Spacer>
+    
+      <Pagination page={page} setPage={setPage} setLimit={setLimit}/>
     </Flex>
   );
 };
